80975ec452b9aafe957c7be7299087ea483ed566
Andrew Shoemaker <andrew.shoemaker9@gmail.com>
2023-12-17 22:46:10 -0600

    started add to cart

--- assets/state.js
+++ assets/state.js
@@ -1,5 +1,14 @@
 import { reactive } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js' 
-export const store = reactive({
+ const store = reactive({
     greeting: 'Hello from store'
 })
 
+ function money(value) {
+    if(!value) return 0;
+    return '$' + (value/100).toFixed(2);
+}
+
+export {
+    store,
+    money
+}
